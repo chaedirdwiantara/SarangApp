04cfe9baeed6b24ca79c850bf6a881b39904201b
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2024-02-08 21:36:58 +0700

    first commit for sarang app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:sarang_app/src/theme_manager/asset_image_icon_manager.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material Boscue'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                  '${AssetImageIconManager.assetPath}/glass_card_image.png'),
+              Image.asset('${AssetImageIconManager.assetPath}/hero_image.png'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
