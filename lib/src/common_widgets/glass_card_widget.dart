6bc83e0ca73c1cbe733e7860527256a119e802b2
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2024-03-01 10:43:44 +0700

    slicing match card

---
+++ lib/src/common_widgets/glass_card_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:sarang_app/src/theme_manager/asset_image_icon_manager.dart';
+import 'package:sarang_app/src/theme_manager/font_manager.dart';
+import 'package:sarang_app/src/theme_manager/style_manager.dart';
+import 'package:sarang_app/src/theme_manager/values_manager.dart';
+
+class GlassCardWidget extends StatelessWidget {
+  const GlassCardWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 80,
+      padding: const EdgeInsets.symmetric(
+        vertical: AppPadding.p15,
+        horizontal: AppPadding.p24,
+      ),
+      margin: const EdgeInsets.symmetric(
+          vertical: AppMargin.m26, horizontal: AppMargin.m30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(33.0),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+              '${AssetImageIconManager.assetPath}/glass_card_image.png'),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Sabrina Anho',
+                style: getWhiteTextStyle(
+                  fontSize: FontSizeManager.f20,
+                  fontWeight: FontWeightManager.semiBold,
+                ),
+              ),
+              Text(
+                '22, Traveler',
+                style: getWhiteTextStyle(
+                  fontSize: FontSizeManager.f14,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    '${AssetImageIconManager.assetPath}/icon_profile.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
