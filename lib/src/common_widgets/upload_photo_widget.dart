da05f9295d383627a744da26ddb06ed01358e7cf
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2024-03-02 14:43:55 +0700

    add image_picker

--- lib/src/common_widgets/upload_photo_widget.dart
+++ lib/src/common_widgets/upload_photo_widget.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sarang_app/src/theme_manager/asset_image_icon_manager.dart';
 import 'package:sarang_app/src/theme_manager/color_manager.dart';
 
 class UploadPhotoWidget extends StatelessWidget {
-  const UploadPhotoWidget({super.key});
+  const UploadPhotoWidget({super.key, required this.image});
+
+  final File? image;
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +26,18 @@ class UploadPhotoWidget extends StatelessWidget {
                   strokeAlign: StrokeAlign.outside)),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(100.0),
-            child: const Image(
-              image: AssetImage(
-                  '${AssetImageIconManager.assetPath}/profile_icon.png'),
-              fit: BoxFit.cover,
-            ),
+            child: image != null
+                ? Image(
+                    image: FileImage(
+                      image!,
+                    ),
+                    fit: BoxFit.cover,
+                  )
+                : const Image(
+                    image: AssetImage(
+                        '${AssetImageIconManager.assetPath}/profile_icon.png'),
+                    fit: BoxFit.cover,
+                  ),
           ),
         ),
         const Positioned(
